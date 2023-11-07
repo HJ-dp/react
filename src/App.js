f6ced00168c418acc0a1aad20c4b080ac987b1d5
HJ-dp <skwo777@hufs.ac.kr>
2023-11-08 04:29:53 +0900

    feat : test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import imgsrc from '../src/3ban.jpg'
 
 function App() {
   return (
     <header>
         <nav>
-            <a href="#">홈</a>
-            <a href="#">헬스장찾기</a>
+            <a href="#">프론트랜드</a>
             <a href="#">마이페이지</a>
-            <a href="#">로그아웃</a>
+            <a href="#">프론트랜드 가입(현재 막힘)</a>
+            <a href="#">그렇게 할거면 프론트랜드 나가</a>
+            <img src={imgsrc} alt="3반"></img>
         </nav>
     </header>
   );
